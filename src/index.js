b5d59cf9d59f309bf88c3a8fe3423337fdad20da
Abdullah ALLALI <contact@allali.me>
2023-01-19 21:42:59 +0100

    [Update] : print solution details and steps animation : index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { parsePuzzle, readFile, generateGoal } from "./utils";
+import { parsePuzzle, readFile, generateGoal, printPuzzle, blok} from "./utils";
 import Node from "./lib/node";
 import Solver from "./lib/solver";
 
@@ -9,6 +9,7 @@ async function main() {
 	const inputFileTxt = await readFile("./src/input");
 	// parse the puzzle from the puzzle into 2d array
 	const puzzle = parsePuzzle(inputFileTxt);
+ 
 	// generated the target puzzle need to be reach (˜the goal puzzle)
 	const goal = generateGoal.snail(puzzle.length);
  
@@ -25,6 +26,26 @@ async function main() {
 
 	// init a solver instant and start the process
 	const solver = new Solver(init);
-	solver.start();
+	let solution = await solver.start();
+
+	const steps = solution.steps
+	const t = {s: solution.time[0], m: parseFloat((solution.time[1]/1000000).toFixed(3))}
+
+	// animate the steps in consol
+	for (let i = 0; i < steps.length; i++) {
+	    console.clear()
+	    printPuzzle(steps[i][0], steps[i][1])
+	    log(`Step n: ${i + 1}/${steps.length}`)
+	    await blok(0.1)
+	}
+
+	// print required detailes about the solution asked in the subject pdf
+	log('---------------------------------------------')
+	log(`	Steps to solution  :`, solution.steps.length)
+	log("	complexity in time :", solution.cTime)
+	log("	complexity in size :", solution.cSize)
+	log("	Total nodes opened :", solution.tONode)
+	log("	Time spent : ", t.s,"s,", t.m, "ms")
+	log('---------------------------------------------')
 }
 main();

4d7b371daa42873dd3d3985295d2a7b9eac51ee5
Abdullah ALLALI <contact@allali.me>
2023-01-25 23:27:35 +0100

    [Update] : edit calls in entry file after new heuristics : index.js

--- src/index.js
+++ src/index.js
@@ -12,25 +12,30 @@ async function main() {
  
 	// generated the target puzzle need to be reach (˜the goal puzzle)
 	const goal = generateGoal.snail(puzzle.length);
- 
+	
 	// set the params which will be used in the solving algorithm
+	// Heuristics Supportedd:
+		// "manhattan", "linearConflicts",  "hamming", 
+		// "euclidean", "diagonal", "gaschnig"
 	const params = {
 		puzzle,
 		greedy: false, // true == ignore the treeLevel score
 		uniform: false, // true == ignore the heuristic score
-		heuristic: "manhattan" // the heuristic to use
+		heuristic: ["gaschnig", "euclidean", "manhattan", "linearConflicts"] // the heuristic to use
 	}
-
+ 
 	// generate new node of the this puzzle which will be the first puzzle in the solver
-	const init = new Node(params.puzzle, params.greedy, params.uniform, goal, undefined, params.heuristic);
+	const initPuzzle = new Node(params.puzzle, params.greedy, params.uniform, goal, undefined, params.heuristic);
+	// display puzzle to terminal
+	printPuzzle(initPuzzle.puzzle, initPuzzle.score)
 
 	// init a solver instant and start the process
-	const solver = new Solver(init);
+	const solver = new Solver(initPuzzle);
 	let solution = await solver.start();
-
+ 
 	const steps = solution.steps
 	const t = {s: solution.time[0], m: parseFloat((solution.time[1]/1000000).toFixed(3))}
-
+ 
 	// animate the steps in consol
 	for (let i = 0; i < steps.length; i++) {
 	    console.clear()
@@ -38,7 +43,7 @@ async function main() {
 	    log(`Step n: ${i + 1}/${steps.length}`)
 	    await blok(0.01)
 	}
-
+	
 	// print required detailes about the solution asked in the subject pdf
 	log('---------------------------------------------')
 	log(`	Steps to solution  :`, solution.steps.length)

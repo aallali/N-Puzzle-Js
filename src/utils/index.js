ca17b67ab5f043355622a008a56f02ff3cfdfa3e
Abdullah ALLALI <contact@allali.me>
2023-03-24 14:08:08 +0000

    [update] : add solvability and puzzgenerator indexs

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,3 +3,5 @@ export { default as parsePuzzle } from "./parsePuzzle.js";
 export { default as generateGoal } from "./goalGenerator.js";
 export { default as printPuzzle } from "./puzzlePrinter";
 export { default as blok } from "./timeBlok";
+export { default as PuzzleGenerator} from "./puzzleGenerator";
+export { default as is_solvable} from "./solvability";

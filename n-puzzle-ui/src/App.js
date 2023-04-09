22d12cc56f8dedfe11fdc80c330c8dcdbfd9035e
Abdullah ALLALI <contact@allali.me>
2023-04-09 01:19:52 +0000

    [feat] : add github repo in bottom

--- n-puzzle-ui/src/App.js
+++ n-puzzle-ui/src/App.js
@@ -6,7 +6,9 @@ function App() {
 
   return (
     <div className="App-container">
-      <PuzzleBoard/>
+      <PuzzleBoard />
+      <p>Github repo if you want to contribute : <a target="_blank" href="https://github.com/aallali/N-Puzzle-Js" rel="noreferrer">https://github.com/aallali/N-Puzzle-Js</a></p>
+
     </div>
   );
 }

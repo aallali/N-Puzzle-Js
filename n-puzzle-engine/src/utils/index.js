4a1d2eb1979cd5aaa1a9dc17ac490c3a23706982
Abdullah ALLALI <contact@allali.me>
2023-04-17 02:19:43 +0000

    [update] : refresh utils index

--- n-puzzle-engine/src/utils/index.js
+++ n-puzzle-engine/src/utils/index.js
@@ -5,4 +5,3 @@ export { default as blok } from "./timeBlok";
 export { default as PuzzleGenerator } from "./puzzleGenerator";
 export { default as is_solvable } from "./solvability";
 export { default as findIndexOf } from "./findIndexOf";
-export { error } from "./errorHandler";

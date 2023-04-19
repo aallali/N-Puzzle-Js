09f355355f9a19f63dec57fc17860f9fdb7271b6
Abdullah ALLALI <contact@allali.me>
2023-04-19 14:49:59 +0000

    [update] change export index to use as deconstructed obj

--- n-puzzle-engine/src/index.js
+++ n-puzzle-engine/src/index.js
@@ -2,4 +2,4 @@ import { PuzzleGenerator, parsePuzzle, generateGoal } from "./utils";
 import Node from "./lib/node";
 import Solver from "./lib/solver";
 
-export default { PuzzleGenerator, parsePuzzle, generateGoal, Node, Solver }
+export { PuzzleGenerator, parsePuzzle, generateGoal, Node, Solver }

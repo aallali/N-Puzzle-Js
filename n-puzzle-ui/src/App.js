6bba139608af1e18b012eeb1b9e73e320ac01fc7
Abdullah ALLALI <contact@allali.me>
2023-04-21 02:49:47 +0000

    [update ] refact code

--- n-puzzle-ui/src/App.js
+++ n-puzzle-ui/src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import PuzzleBoard from './puzzle/Broad';
 
 function App() {
-
+  // console.log(window)
+  const githubRepo = "https://github.com/aallali/N-Puzzle-Js"
   return (
     <div className="App-container">
       <PuzzleBoard />
-      <p>Github repo if you want to contribute : <a target="_blank" href="https://github.com/aallali/N-Puzzle-Js" rel="noreferrer">https://github.com/aallali/N-Puzzle-Js</a></p>
-
+      <p>Github repo if you want to contribute : <a target="_blank" href={githubRepo} rel="noreferrer">{githubRepo}</a></p>
     </div>
   );
 }

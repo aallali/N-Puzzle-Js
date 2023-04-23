43670ad911b127cf966da527f69b53f07b5c6c71
Abdullah ALLALI <contact@allali.me>
2023-04-24 00:17:36 +0100

    [update] : limit solver iterations to 200k max : UI

--- n-puzzle-ui/public/worker.js
+++ n-puzzle-ui/public/worker.js
@@ -11,14 +11,18 @@ self.onmessage = (e) => {
   const solver = new Solver(data.qType)
   data.acknowledged = true;
 
+
   if (solver.checkSolvability(initPuzz.puzzle, initPuzz.goal)) {
-    solver[`start_${data.algorithm}`](Infinity, initPuzz).then((solution) => {
-      self.postMessage(solution)
+
+    solver[`start_${data.algorithm}`](data.goal.length == 3 ? Infinity : 200000, initPuzz).then((solution) => {
+      if (!solution || solution == -1)
+        self.postMessage({ error: `sorry couldnt reach a solution for this, this is highly due to deep recursion/iteration limit (200000), you still can open an issue about it on github repo. including the exact puzzle and search options chosen to optimize the algorithm.` });
+      else
+        self.postMessage(solution)
     })
 
   } else {
-    console.log()
-    self.postMessage({ error: `unsolvable puzzle to ${data.goal} state, try change the goal or the puzzle itself. ` });
+    self.postMessage({ error: `[unsolvable puzzle to ${data.goal} state, try change the goal or the puzzle itself.]` });
   }
 
 };
